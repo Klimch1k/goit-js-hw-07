c3f3644c21667fd53b37d31cd0f5b9789af6fdad
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-03-04 00:24:33 +0200

    task-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Створення розмітки галереї
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(
+      (image) => `<li><a class="gallery__item" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+  />
+</a></li>`
+    )
+    .join("");
+}
+
+
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  scrollZoom: false,
+  captionsData: "alt",
+  captionDelay: '250'
+});
+
+
